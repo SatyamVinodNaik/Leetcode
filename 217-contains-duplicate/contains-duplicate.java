153f11079a1b3b705389ae3df198e442ba2f296f
Satyam Vinod Naik <satyvinod789@gmail.com>
2026-08-12 22:38:39 +0530

    Time: 24 ms (28.40%) | Memory: 81.4 MB (86.26%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int i = 0;
+        int j = 1;
+
+        while (j < nums.length) {
+
+            if (nums[i] == nums[j]) {
+                return true;
+            }
+
+            i++;
+            j++;
+        }
+
+        return false;
+    }
+}
